d13748e43e2e5664cd765f7769e4019b482d0f03
ruyang0621 <71616956+yangr2@users.noreply.github.com>
2022-03-24 16:16:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ app.get('/', (req, res) => {
     res.send("Welcome to the home page!");
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
+//const port = process.env.PORT || 3000;
+app.listen(PORT, () => {
     console.log("Hello World");
 });
 
